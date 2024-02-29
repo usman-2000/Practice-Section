da7d82290b3d8b32cc0e42ca0e7bc2cf62533ca0
usman-2000 <usmanrahim2000@gmail.com>
2024-02-29 16:25:39 +0500

    Hacker Rank Question

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,49 +1,49 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./styles.css"
 const Navbar = () => {
-    const [list1, setList1] = useState([{
-        id: 1,
-        name: "Usman",
-        age: 23
-    },
-    {
-        id: 2,
-        name: "Sami",
-        age: 23
-    }, {
-        id: 3,
-        name: "Waqar",
-        age: 23
-    },
-    {
-        id: 4,
-        name: "Zain",
-        age: 23
-    },
-    {
-        id: 5,
-        name: "Khalid",
-        age: 23
-    }])
-    const [list2, setList2] = useState([
-        {
-            id: 2,
-            worked_hours: 20
-        },
-        {
-            id: 3,
-            worked_hours: 2
-        },
+    // const [list1, setList1] = useState([{
+    //     id: 1,
+    //     name: "Usman",
+    //     age: 23
+    // },
+    // {
+    //     id: 2,
+    //     name: "Sami",
+    //     age: 23
+    // }, {
+    //     id: 3,
+    //     name: "Waqar",
+    //     age: 23
+    // },
+    // {
+    //     id: 4,
+    //     name: "Zain",
+    //     age: 23
+    // },
+    // {
+    //     id: 5,
+    //     name: "Khalid",
+    //     age: 23
+    // }])
+    // const [list2, setList2] = useState([
+    //     {
+    //         id: 2,
+    //         worked_hours: 20
+    //     },
+    //     {
+    //         id: 3,
+    //         worked_hours: 2
+    //     },
 
-        {
-            id: 5,
-            worked_hours: 5
-        }
-    ])
+    //     {
+    //         id: 5,
+    //         worked_hours: 5
+    //     }
+    // ])
 
-    const [list3, setList3] = useState(null)
+    // const [list3, setList3] = useState(null)
 
-    const newArr = []
+    // const newArr = []
 
     // list1.map((d) => {
     //     list2.map((f) => {
@@ -64,14 +64,50 @@ const Navbar = () => {
     //     newArr.push(fil)
     // })
 
-    list1.map((d) => {
-        for (let i = 0; i < list2.length; i++) {
+    // list1.map((d) => {
+    //     for (let i = 0; i < list2.length; i++) {
 
+    //     }
+    // })
+
+    // console.log(list3, "listed3")
+
+
+    var library = [
+        {
+            title: 'Bill Gates',
+            author: 'The Road Ahead',
+            readingStatus: true
+        },
+        {
+            title: 'Steve Jobs',
+            author: 'Walter Isaacson',
+            readingStatus: true
+        },
+        {
+            title: 'Mockingjay: The Final Book of The Hunger Games',
+            author: 'Suzanne Collins',
+            readingStatus: false
         }
-    })
+    ];
 
-    console.log(list3, "listed3")
 
+    const [newArr, setNewArr] = useState([])
+    const read = () => {
+        for (let i = 0; i < library.length; i++) {
+            if (library[i].readingStatus === true) {
+                console.log(`Already Read ${library[i].title} by ${library[i].author}`)
+                setNewArr({ ...newArr, title: library[i], author: library[i] })
+            }
+        }
+    }
+
+    console.log(newArr, "New Arr")
+    // read()
+
+    useEffect(() => {
+        read()
+    }, [])
 
 
 
@@ -94,6 +130,12 @@ const Navbar = () => {
 
             </div> */}
             <h1>Hello</h1>
+            {/* <ul>
+                {newArr.map((i) => {
+                    return <li>Already Read {i.title} by {i.author}</li>
+                })}
+            </ul> */}
+            <button onClick={read}>Click</button>
         </div>
     )
 }
