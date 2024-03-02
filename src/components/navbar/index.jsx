a28e2613b6a02b0f64f65dbe0a69b0949ddbd7cf
usman-2000 <usmanrahim2000@gmail.com>
2024-03-02 17:10:55 +0500

    FIZZ BUZZ HACKERRANK

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -73,6 +73,25 @@ const Navbar = () => {
     // console.log(list3, "listed3")
 
 
+    // FIZZBUZZ SCENARIO HACKERRANK
+    // function fizzBuzz(n) {
+    //     // Write your code here
+    //     for (let i = 1; i <= n; i++) {
+    //         if (i % 3 == 0 && i % 5 == 0) {
+    //             console.log("FizzBuzz")
+    //         } else if (i % 3 == 0 && i % 5 != 0) {
+    //             console.log("Fizz")
+    //         } else if (i % 3 != 0 && i % 5 == 0) {
+    //             console.log("Buzz")
+    //         } else if (i % 3 != 0 && i % 5 != 0) {
+    //             console.log(i)
+    //         }
+    //     }
+
+
+    // }
+
+
     var library = [
         {
             title: 'Bill Gates',
