c74cd324e8d37b5a10d7ed2c4ffa51bc187f7f85
usman-2000 <usmanrahim2000@gmail.com>
2024-04-30 01:47:04 +0500

    cons

--- foundry-23/src/BasicNft.sol
+++ foundry-23/src/BasicNft.sol
@@ -3,4 +3,15 @@ pragma solidity 0.8.19;
 
 import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract BasicNFT is ERC721{}
+contract BasicNFT is ERC721{
+
+    uint256 private t_counter;
+
+    constructor() ERC721("BasicNFT","B_NFT"){
+        t_counter = 0;
+    }
+
+    function mintNft(type name) {
+        
+    }
+}
